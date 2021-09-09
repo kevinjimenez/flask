3bd78d9c8b683eb60956db50cb1e4150e0e043fe
kevin jimenez <kivito2009@hotmail.com>
2021-09-09 10:20:00 -0500

    sentencias de control

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO1', 'TODO2', 'TODO3']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -14,7 +16,12 @@ def index():
 def hello():
     user_ip = request.cookies.get('user_ip')
     # return 'hello world platzi '+user_ip
-    return render_template('hello.html', user_ip=user_ip)
+    # VARIABLES PARA PASAR AL TEMPLATE ES EL SEGUNDO PARAMETREO
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+    return render_template('hello.html', **context)
 
 
 if __name__ == '__main__':
